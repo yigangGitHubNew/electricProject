526e48cd0ee97d8088920df7b2c795ff0d7d82cf
Administrator <yigang_wu@126.com>
2017-12-15 15:03:56 +0800

    add

---
+++ springboot-helloworld/src/main/java/org/spring/springboot/aop/AopConfig.java
@@ -0,0 +1,52 @@
+package org.spring.springboot.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 接收请求报价消息的测试类
+ *
+ * @author yigang.wu
+ * @date created in ${time} ${date}
+ */
+@Aspect
+@Configuration
+public class AopConfig {
+    @Pointcut("execution(* org.spring.springboot.web.HelloWorldController.sayHello(..))")
+    public void excudeService(){}
+
+    @Before("excudeService()")
+    public void before() {
+        System.out.println("切面before执行了");
+    }
+
+    @After("excudeService()")
+    public void after() {
+        System.out.println("切面after执行了");
+    }
+
+    @AfterReturning("excudeService()")
+    public void afterReturning() {
+        System.out.println("切面afterReturning执行了");
+    }
+
+    @AfterThrowing("excudeService()")
+    public void afterThrowing() {
+        System.out.println("切面afterThrowing执行了");
+    }
+
+    @Around("excudeService()")
+    public Object around(ProceedingJoinPoint thisJoinPoint){
+        Object obj = null;
+        System.err.println ("切面around before执行了");
+        try {
+            thisJoinPoint.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace ();
+        }
+        System.err.println ("切面around after执行了");
+        return obj;
+    }
+
+}
