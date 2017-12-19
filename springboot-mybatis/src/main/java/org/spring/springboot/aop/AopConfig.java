ee4a5b169608a753bdec078004fa533643fa6fc9
Administrator <yigang_wu@126.com>
2017-12-19 19:50:00 +0800

    aop中@around注解的使用

--- springboot-mybatis/src/main/java/org/spring/springboot/aop/AopConfig.java
+++ springboot-mybatis/src/main/java/org/spring/springboot/aop/AopConfig.java
@@ -3,6 +3,7 @@ package org.spring.springboot.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 /**
  * 接收请求报价消息的测试类
@@ -16,7 +17,7 @@ public class AopConfig {
     @Pointcut("execution(* org.spring.springboot.service.CityService.findCityByName(..))")
     public void excudeService(){}
 
-    @Before("excudeService()")
+   /* @Before("excudeService()")
     public void before() {
         System.out.println("切面before执行了");
     }
@@ -34,22 +35,23 @@ public class AopConfig {
     @AfterThrowing("excudeService()")
     public void afterThrowing() {
         System.out.println("切面afterThrowing执行了");
-    }
+    }*/
 
     @Around("excudeService()")
     public Object around(ProceedingJoinPoint thisJoinPoint){
         Object obj = null;
-        System.err.println ("切面around before执行了");
+        System.out.println ("切面around before执行了");
         try {
-            Object[] agrs = thisJoinPoint.getArgs();
-            for (Object o : agrs){
-                System.out.println(o.toString());
+            Object[] objs = thisJoinPoint.getArgs();
+            if(StringUtils.isEmpty(objs[0].toString())){
+                objs[0] = "abc";
             }
-            thisJoinPoint.proceed();
+            System.out.println("the agrs is:"+objs[0].toString());
+            obj = thisJoinPoint.proceed();
         } catch (Throwable e) {
             e.printStackTrace ();
         }
-        System.err.println ("切面around after执行了");
+        System.out.println ("切面around after执行了");
         return obj;
     }
 
